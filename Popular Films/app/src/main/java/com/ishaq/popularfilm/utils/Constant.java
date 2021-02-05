d81f0ecfeef3755b80986247d71d3f7ac0a50bea
Ishaq A Mohamed <mohamedishaq1994@gmail.com>
2021-02-05 18:57:09 +0400

    first commit

---
+++ Popular Films/app/src/main/java/com/ishaq/popularfilm/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.ishaq.popularfilm.utils;
+
+public class Constant {
+
+    public static String IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+    // Constant Variables
+    public static String MOVIE = "movie";
+
+}
