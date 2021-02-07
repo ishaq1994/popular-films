88056f1098514df58266015677f4672994b2c1b8
Ishaq A Mohamed <mohamedishaq1994@gmail.com>
2021-02-08 00:28:15 +0400

    update

--- Popular Films/app/src/test/java/com/ishaq/popularfilm/UtilityTest.java
+++ Popular Films/app/src/test/java/com/ishaq/popularfilm/UtilityTest.java
@@ -9,12 +9,13 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *
  */
 public class UtilityTest {
 
     @Test
     public void formatDateTest(){
+
 
         String formatDate= Utility.formatDate("2020-11-19");
         assertEquals("19-11-2020",formatDate);
